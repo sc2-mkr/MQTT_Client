cb6ad2e3da01b86bc79013ecb5e6d969a846f423
Iregon <0alsidavi0>
2019-06-16 17:48:43 +0200

    Update logger service and logs text area

--- src/main/java/services/utils/logs/Logger.java
+++ src/main/java/services/utils/logs/Logger.java
@@ -2,17 +2,18 @@ package services.utils.logs;
 
 import java.util.ArrayList;
 
-public class Logger implements LoggerFactory{
+public class Logger implements LoggerFactory {
 
     private static Logger instance = new Logger();
 
     private ArrayList<LoggerFactory> loggers = new ArrayList<>();
 
+    private Logger() {
+    }
+
     public static Logger getInstance() {
         return instance;
     }
-
-    private Logger() {}
 
     public void addLogger(LoggerFactory logger) {
         loggers.add(logger);
--- src/main/java/services/utils/logs/LoggerFactory.java
+++ src/main/java/services/utils/logs/LoggerFactory.java
@@ -2,5 +2,6 @@ package services.utils.logs;
 
 public interface LoggerFactory {
     void log(String msg);
+
     void logError(String msg);
 }
