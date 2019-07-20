f2aefb49b5468cfebeac48b35d3333c6507e2f95
Iregon <0alsidavi0>
2019-07-20 12:03:22 +0200

    Add publishing topic feature

---
+++ src/main/java/services/mqtt/MqttPublisher.java
@@ -0,0 +1,33 @@
+package services.mqtt;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MqttPublisher implements Runnable {
+
+    private MqttMessageExtended msg;
+    private int interval;
+    private boolean isLoop;
+
+    private Timer timer = new Timer();
+    private TimerTask task;
+
+    public MqttPublisher(MqttMessageExtended msg, int interval, boolean isLoop) {
+        this.msg = msg;
+        this.interval = interval;
+        this.isLoop = isLoop;
+
+        task = new MqttTask(msg);
+    }
+
+    @Override
+    public void run() {
+        if(isLoop) timer.schedule(task, 0, interval);
+        else timer.schedule(task, 0);
+    }
+
+    public void stop() {
+        timer.cancel();
+    }
+}
+
---
+++ src/main/java/services/mqtt/MqttPublishersManager.java
@@ -0,0 +1,33 @@
+package services.mqtt;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import services.utils.logs.Logger;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+
+public class MqttPublishersManager {
+
+    private ArrayList<MqttPublisher> publishers = new ArrayList<>();
+
+    public MqttPublishersManager(MqttClient client) {
+        MqttMessageSender.getInstance().setClient(client);
+    }
+
+    public void addPublisher(MqttMessageExtended msg, int interval, boolean isLoop) {
+        MqttPublisher pub = new MqttPublisher(msg, interval, isLoop);
+        publishers.add(pub);
+        Thread t = new Thread(pub);
+        printLog(msg, interval, isLoop);
+        t.start();
+    }
+
+    private void printLog(MqttMessageExtended mqttMsg, int interval, boolean isLoop) {
+        String msg = MessageFormat.format("Added publisher: Topic: {0}, Payload: {1}", mqttMsg.getTopic(), mqttMsg.getPayloadString());
+        Logger.getInstance().logInfo(msg);
+    }
+
+    public void stopAllPublishers() {
+        publishers.stream().forEach((pub) -> pub.stop());
+    }
+}
