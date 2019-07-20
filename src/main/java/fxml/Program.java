8839863281a5275e7afe525ec94705a7b059a9d8
Iregon <0alsidavi0>
2019-07-20 12:05:29 +0200

    Manage window closing

--- src/main/java/fxml/Program.java
+++ src/main/java/fxml/Program.java
@@ -1,7 +1,8 @@
 package fxml;
 
 import configs.Configuration;
-import fxml.controllers.WindowEventManager;
+import fxml.controllers.ProgramController;
+import fxml.controllers.WindowEventHandler;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
@@ -14,6 +15,7 @@ import java.io.IOException;
 public class Program extends Application {
 
     private Parent rootNode;
+    private ProgramController controller;
 
     public static void main(String[] args) {
         Application.launch(args);
@@ -30,17 +32,18 @@ public class Program extends Application {
             FXMLLoader fxmlLoader;
             fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/Program.fxml"));
             rootNode = fxmlLoader.load();
+            controller = fxmlLoader.getController();
         } catch (NullPointerException e) {
-            System.err.println("Program - NullPointerException: " + e.getMessage());
+            System.err.println("Program - NullPointerException: " + e.getMessage()); // TODO use logger
         } catch (IOException e) {
-            System.err.println("Program - IOException: " + e.getMessage());
+            System.err.println("Program - IOException: " + e.getMessage()); // TODO use logger
         }
     }
 
     @Override
     public void start(Stage primaryStage) {
         // Manage window closure
-        primaryStage.setOnCloseRequest(e -> WindowEventManager.windowClosing());
+        primaryStage.setOnCloseRequest(e -> controller.windowClosing());
 
         primaryStage.setScene(new Scene(rootNode));
         //primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/resources/icon.png")));
@@ -48,5 +51,10 @@ public class Program extends Application {
         primaryStage.setMinHeight(new Double(Configuration.getInstance().getValue("minWindowHeight")));
         primaryStage.setMinWidth(new Double(Configuration.getInstance().getValue("minWindowWidth")));
         primaryStage.show();
+    }
+
+    @Override
+    public void stop(){
+
     }
 }
