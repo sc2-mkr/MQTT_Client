694f9af94a110f9fa835d1c78208239341ad3d26
Iregon <0alsidavi0>
2019-08-21 14:05:37 +0200

    Add alert message utils

---
+++ src/main/java/services/utils/fxUtils/AlertUtil.java
@@ -0,0 +1,30 @@
+package services.utils.fxUtils;
+
+import javafx.scene.control.Alert;
+
+public class AlertUtil {
+    private static AlertUtil instance = new AlertUtil();
+
+    public static AlertUtil getInstance() {
+        return instance;
+    }
+
+    private AlertUtil() {
+    }
+
+    public void showAlertAndWait(Alert.AlertType type, String title, String header, String content) {
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+    public void showWarningAndWait(String title, String header, String content) {
+        showAlertAndWait(Alert.AlertType.WARNING, title, header, content);
+    }
+
+    public void showErrorandWait(String title, String header, String content) {
+        showAlertAndWait(Alert.AlertType.ERROR, title, header, content);
+    }
+}
