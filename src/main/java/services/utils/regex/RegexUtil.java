708e2c04b26cbd106e7b5a603706c66cfb9dff7f
Iregon <0alsidavi0>
2019-08-27 20:14:00 +0200

    Bugfix

--- src/main/java/services/utils/regex/RegexUtil.java
+++ src/main/java/services/utils/regex/RegexUtil.java
@@ -1,5 +1,6 @@
 package services.utils.regex;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexUtil {
@@ -14,6 +15,17 @@ public class RegexUtil {
 
     public boolean match(String pattern, String value) {
         Pattern p = Pattern.compile(pattern);
-        return p.matcher(value).matches();
+        Matcher m = p.matcher(value);
+
+        /**
+         * p.matcher(value).matches() method doesn't work in this case because it check the pattern
+         * on all over the value.
+         * Example:
+         * pattern: *pattern for random expression*
+         * value: {RANDOM(1,50)} -> return: true
+         * value: n.{RANDOM(1,50)} -> return: false
+         */
+        if(m.find()) return true;
+        else return false;
     }
 }
